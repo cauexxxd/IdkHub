66b369673db92a5c176eab08e4adb64a0606f6aa
Cauê.zx <caue61075@gmail.com>
2025-08-26 19:26:02 -0300

    Update IdkHub.lua

--- IdkHub.lua
+++ IdkHub.lua
@@ -36,4 +36,4 @@ local Window = Rayfield:CreateWindow({
       Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
    }
 }) 
-local mainTab = Window:CreateTab("🏡Home",nil) -- Title, Image
+local mainTab = Window:CreateTab("🏡Home", 4483362458) -- Title, Image
