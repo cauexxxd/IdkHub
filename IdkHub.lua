3249dfe02159345c2a4b858295c6c6d3dc11a0d5
Cauê.zx <caue61075@gmail.com>
2025-08-27 01:42:16 -0300

    Update IdkHub.lua

--- IdkHub.lua
+++ IdkHub.lua
@@ -1,39 +1,127 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
-   Name = "idk Hub",
-   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
-   LoadingTitle = "Rayfield Interface Suite",
+   Name = "Blade Ball Hub",
+   Icon = 0,
+   LoadingTitle = "Blade Ball Hub",
    LoadingSubtitle = "by cauezin",
-   ShowText = "Rayfield", -- for mobile users to unhide rayfield, change if you'd like
-   Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
+   ShowText = "Rayfield",
+   Theme = "Default",
+   ToggleUIKeybind = "K",
+})
 
-   ToggleUIKeybind = "K", -- The keybind to toggle the UI visibility (string like "K" or Enum.KeyCode)
+local mainTab = Window:CreateTab("🏡Home", 4483362458)
 
-   DisableRayfieldPrompts = false,
-   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
+-- Variáveis
+local autoParry = false
+local autoSpam = false
+local spamSpeed = 0.5
+local parryDist = 30
+local autoAbility = false
 
-   ConfigurationSaving = {
-      Enabled = true,
-      FolderName = nil, -- Create a custom folder for your hub/game
-      FileName = "Big Hub"
-   },
+-- Função AutoParry
+function AutoParryFunc()
+    while autoParry do
+        pcall(function()
+            local player = game.Players.LocalPlayer
+            local char = player.Character
+            if char and char:FindFirstChild("HumanoidRootPart") then
+                for _, ball in pairs(workspace:GetChildren()) do
+                    if ball.Name == "Ball" and ball:FindFirstChild("Velocity") then
+                        local dist = (char.HumanoidRootPart.Position - ball.Position).Magnitude
+                        if dist < parryDist then
+                            -- Parry remoto do Blade Ball
+                            game:GetService("ReplicatedStorage").Remotes.Parry:FireServer()
+                        end
+                    end
+                end
+            end
+        end)
+        task.wait(0.05)
+    end
+end
 
-   Discord = {
-      Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
-      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
-      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
-   },
+-- Função AutoSpam
+function AutoSpamFunc()
+    while autoSpam do
+        pcall(function()
+            game:GetService("ReplicatedStorage").Remotes.Parry:FireServer()
+        end)
+        task.wait(spamSpeed)
+    end
+end
 
-   KeySystem = false, -- Set this to true to use our key system
-   KeySettings = {
-      Title = "Untitled",
-      Subtitle = "Key System",
-      Note = "No method of obtaining the key is provided", -- Use this to tell the user how to get a key
-      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
-   }
-}) 
-local mainTab = Window:CreateTab("🏡Home", 4483362458) -- Title, Image
+-- Função Manual Spam
+function ManualSpamFunc()
+    game:GetService("ReplicatedStorage").Remotes.Parry:FireServer()
+end
+
+-- Função AutoAbility
+function AutoAbilityFunc()
+    while autoAbility do
+        pcall(function()
+            game:GetService("ReplicatedStorage").Remotes.Ability:FireServer()
+        end)
+        task.wait(1)
+    end
+end
+
+-- UI Rayfield Toggles/Sliders/Buttons
+
+mainTab:CreateToggle({
+    Name = "AutoParry",
+    CurrentValue = false,
+    Callback = function(Value)
+        autoParry = Value
+        if Value then
+            spawn(AutoParryFunc)
+        end
+    end
+})
+
+mainTab:CreateToggle({
+    Name = "Auto Spam",
+    CurrentValue = false,
+    Callback = function(Value)
+        autoSpam = Value
+        if Value then
+            spawn(AutoSpamFunc)
+        end
+    end
+})
+
+mainTab:CreateSlider({
+    Name = "Spam Speed (menor = mais rápido)",
+    Range = {0.05, 2},
+    Increment = 0.05,
+    CurrentValue = spamSpeed,
+    Callback = function(Value)
+        spamSpeed = Value
+    end
+})
+
+mainTab:CreateButton({
+    Name = "Spam Manual",
+    Callback = ManualSpamFunc
+})
+
+mainTab:CreateSlider({
+    Name = "Parry Accuracy (distância)",
+    Range = {5, 50},
+    Increment = 1,
+    CurrentValue = parryDist,
+    Callback = function(Value)
+        parryDist = Value
+    end
+})
+
+mainTab:CreateToggle({
+    Name = "Auto Ability",
+    CurrentValue = false,
+    Callback = function(Value)
+        autoAbility = Value
+        if Value then
+            spawn(AutoAbilityFunc)
+        end
+    end
+})
