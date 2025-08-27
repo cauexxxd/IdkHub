0f74cd115928cead0c45bff82449591d7d2823b5
Cauê.zx <caue61075@gmail.com>
2025-08-26 21:40:02 -0300

    Atualizar o IdkHub.lua

--- IdkHub.lua
+++ IdkHub.lua
@@ -1,5 +1,6 @@
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
-
+if game.Placeld  then
+local Rayfield =
+ loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 local Window = Rayfield:CreateWindow({
    Name = "idk Hub",
    Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
