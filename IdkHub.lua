1fa308400eb154c735ee0dff6b258334bf996398
Cauê.zx <caue61075@gmail.com>
2025-08-26 22:11:22 -0300

    Update IdkHub.lua

--- IdkHub.lua
+++ IdkHub.lua
@@ -47,3 +47,11 @@ Rayfield:Notify({
    Duration = 5,
    Image = nil,
 })
+
+
+local Button = Tab:CreateButton({
+   Name = "Button Example",
+   Callback = function()
+   -- The function that takes place when the button is pressed
+   end,
+})
