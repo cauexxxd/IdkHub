69549775f75017ef5b215c8a9a3512b7b7ff6ba3
Cauê.zx <caue61075@gmail.com>
2025-08-26 21:35:12 -0300

    Update IdkHub.lua

--- IdkHub.lua
+++ IdkHub.lua
@@ -39,30 +39,3 @@ local Window = Rayfield:CreateWindow({
 local mainTab = Window:CreateTab("🏡Home", 4483362458) -- Title, Image
 local MainSection MainTab: CreateSection("Main") 
 
-Rayfield: Notify({
-
-Title "You executed the script!",
-
-Content "very good gui",
-
-Duration 5,
-
-Image nil,
-
-Actions { Notification Buttons
-
-Ignore = {
-
-Name "Okay!",
-
-Callback function()
-
-print("The user tapped Okay!")
-
-end
-
-},
-
-},
-
-})
